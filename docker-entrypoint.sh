04a0190e16b34ed0ce413d76e98cb2e20a57ee04
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2016-09-22 15:41:48 +0200

    added gitattributes gitignore and dockerignore files

---
+++ docker-entrypoint.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+set -e
+local MYUSER="pyload"
+local MYGID="10005"
+local MYUID="10005"
+
+ConfigureSsmtp () {
+  # Customizing sstmp
+  if [ -f /etc/ssmtp/ssmtp.conf ];then
+    # Configure relay
+    if [ -n "${DOCKRELAY}" ]; then
+      /bin/sed -i "s|mailhub=mail|mailhub=${DOCKRELAY}|i" /etc/ssmtp/ssmtp.conf
+    fi
+    # Configure root
+    if [ -n "${DOCKMAIL}" ]; then
+      /bin/sed -i "s|root=postmaster|root=${DOCKMAIL}|i" /etc/ssmtp/ssmtp.conf
+    fi
+    # Configure domain
+    if [ -n "${DOCKMAILDOMAIN}" ]; then
+      /bin/sed -i "s|#rewriteDomain=.*|rewriteDomain=${DOCKMAILDOMAIN}|i" /etc/ssmtp/ssmtp.conf
+    fi
+  fi
+}
+
+ConfigureUser () {
+  # Managing user
+  if [ -n "${DOCKUID}" ]; then
+    MYUID="${DOCKUID}"
+  fi
+  # Managing group
+  if [ -n "${DOCKGID}" ]; then
+    MYGID="${DOCKGID}"
+  fi
+  local OLDHOME
+  local OLDGID
+  local OLDUID
+  local USEREXISTS=$(/bin/grep -q "${MYUSER}" /etc/passwd)
+  if [ $USEREXISTS -eq 0 ]; then
+    OLDUID=$(/usr/bin/id -u "${MYUSER}")
+    OLDGID=$(/usr/bin/id -g "${MYUSER}")
+    if [ "${DOCKUID}" != "${OLDUID}" ]; then
+      OLDHOME=$(/bin/echo "~${MYUSER}")
+      /usr/sbin/deluser "${MYUSER}"
+      /usr/bin/logger "Deleted user ${MYUSER}"
+    fi
+    local GROUPEXISTS=$(/bin/grep -q "${MYUSER}" /etc/group)
+    if [ $GROUPEXISTS -eq 0 ]; then
+      local OLDGID=$(/usr/bin/id -g "${MYUSER}")
+      if [ "${DOCKGID}" != "${OLDGID}" ]; then
+        /usr/sbin/delgroup "${MYUSER}"
+        /usr/bin/logger "Deleted group ${MYUSER}"
+      fi
+    fi
+  fi
+  /usr/sbin/addgroup -S -g "${MYGID}" "${MYUSER}"
+  /usr/sbin/adduser -S -D -H -s /sbin/nologin -G "${MYUSER}" -h "${OLDHOME}" -u "${MYUID}" "${MYUSER}"
+  if [ -n "${OLDUID}" ] && [ "${DOCKUID}" != "${OLDUID}" ]; then
+    /usr/bin/find / -user "${OLDUID}" -exec /bin/chown ${MYUSER} {} \;
+  fi
+  if [ -n "${OLDGID}" ] && [ "${DOCKGID}" != "${OLDGID}" ]; then
+    /usr/bin/find / -group "${OLDGID}" -exec /bin/chgrp ${MYUSER} {} \;
+  fi
+}
+
+ConfigureUser
+ConfigureSsmtp
+
+if [ "$1" = 'pyload' ]; then
+    if [ -e /config/pyload.pid ]; then
+      /bin/rm /config/pyload.pid
+    fi
+    /bin/chown -R "${MYUSER}":"${MYUSER}" /config 
+    /bin/chmod -R g+w /config /config
+    exec /sbin/su-exec "${MYUSER}" /usr/bin/python /opt/pyload/pyLoadCore.py --configdir=/config "$@"
+fi
+
+exec "$@"
