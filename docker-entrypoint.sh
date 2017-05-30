38a8341b27a8a662bb7631407da558a743e85cc7
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2017-05-30 08:20:41 +0200

    added autoupgrade and updated configureuser feature

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,21 +3,30 @@
 MYUSER="pyload"
 MYGID="10005"
 MYUID="10005"
+OS=""
+
+DectectOS(){
+  if [ -e /etc/alpine-release ]; then
+    OS="alpine"
+  elif [ -e /etc/os-release ]; then
+    if /bin/grep -q "NAME=\"Ubuntu\"" /etc/os-release ; then
+      OS="ubuntu"
+    fi
+  fi
+}
 
 AutoUpgrade(){
-  if [ -e /etc/alpine-release ]; then
+  if [ "${OS}" == "alpine" ]; then
     /sbin/apk --no-cache upgrade
     /bin/rm -rf /var/cache/apk/*
-  elif [ -e /etc/os-release ]; then
-    if /bin/grep -q "NAME=\"Ubuntu\"" /etc/os-release ; then 
-      export DEBIAN_FRONTEND=noninteractive
-      /usr/bin/apt-get update
-      /usr/bin/apt-get -y --no-install-recommends dist-upgrade
-      /usr/bin/apt-get -y autoclean
-      /usr/bin/apt-get -y clean 
-      /usr/bin/apt-get -y autoremove
-      /bin/rm -rf /var/lib/apt/lists/*
-    fi
+  elif [ "${OS}" == "ubuntu" ]; then
+    export DEBIAN_FRONTEND=noninteractive
+    /usr/bin/apt-get update
+    /usr/bin/apt-get -y --no-install-recommends dist-upgrade
+    /usr/bin/apt-get -y autoclean
+    /usr/bin/apt-get -y clean
+    /usr/bin/apt-get -y autoremove
+    /bin/rm -rf /var/lib/apt/lists/*
   fi
 }
 
@@ -51,17 +60,15 @@ ConfigureUser () {
   local OLDHOME
   local OLDGID
   local OLDUID
-  /bin/grep -q "${MYUSER}" /etc/passwd
-  if [ $? -eq 0 ]; then
+  if /bin/grep -q "${MYUSER}" /etc/passwd; then
     OLDUID=$(/usr/bin/id -u "${MYUSER}")
     OLDGID=$(/usr/bin/id -g "${MYUSER}")
     if [ "${DOCKUID}" != "${OLDUID}" ]; then
-      OLDHOME=$(/bin/echo "~${MYUSER}")
+      OLDHOME=$(/bin/grep "$MYUSER" /etc/passwd | /usr/bin/awk -F: '{print $6}')
       /usr/sbin/deluser "${MYUSER}"
       /usr/bin/logger "Deleted user ${MYUSER}"
     fi
-    /bin/grep -q "${MYUSER}" /etc/group
-    if [ $? -eq 0 ]; then
+    if /bin/grep -q "${MYUSER}" /etc/group; then
       local OLDGID=$(/usr/bin/id -g "${MYUSER}")
       if [ "${DOCKGID}" != "${OLDGID}" ]; then
         /usr/sbin/delgroup "${MYUSER}"
@@ -69,8 +76,12 @@ ConfigureUser () {
       fi
     fi
   fi
-  /usr/sbin/addgroup -S -g "${MYGID}" "${MYUSER}"
-  /usr/sbin/adduser -S -D -H -s /sbin/nologin -G "${MYUSER}" -h "${OLDHOME}" -u "${MYUID}" "${MYUSER}"
+  if ! /bin/grep -q "${MYUSER}" /etc/group; then
+    /usr/sbin/addgroup -S -g "${MYGID}" "${MYUSER}"
+  fi
+  if ! /bin/grep -q "${MYUSER}" /etc/passwd; then
+    /usr/sbin/adduser -S -D -H -s /sbin/nologin -G "${MYUSER}" -h "${OLDHOME}" -u "${MYUID}" "${MYUSER}"
+  fi
   if [ -n "${OLDUID}" ] && [ "${DOCKUID}" != "${OLDUID}" ]; then
     /usr/bin/find / -user "${OLDUID}" -exec /bin/chown ${MYUSER} {} \;
   fi
@@ -79,9 +90,9 @@ ConfigureUser () {
   fi
 }
 
+DectectOS
 AutoUpgrade
 ConfigureUser
-ConfigureSsmtp
 
 if [ "$1" = 'pyload' ]; then
     if [ -d /config ]; then
