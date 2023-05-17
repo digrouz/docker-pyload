6e61dc74e8383d3a772694729f9892feb1f37b83
DI GREGORIO Nicolas <digrouz@jedha.digrouz.local>
2023-05-17 15:26:12 +0200

    notification script

--- root/usr/local/bin/pyload-download-finished-mail-notification.sh
+++ root/usr/local/bin/pyload-download-finished-mail-notification.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/command/with-contenv sh
+
 mail -s "Pyload: Download Finished" root <<EOF
 ${2} was finished at $(date +"%H:%M") on $(date +"%d.%m.%y"). ...forwarding it now to the extraction queue.
 EOF
